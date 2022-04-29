999dd2cf92835c5195823716ba2326c81780aedd
Javier Galindo <javier.galindot@gmail.com>
2022-04-29 16:56:13 +0300

    version 1 lab 2

---
+++ modules/galindo_30_rule/include/galindo_30_rule.h
@@ -0,0 +1,32 @@
+// Copyright 2022 Javier Galindo
+
+#ifndef MODULES_30_RULE_INCLUDE_30_RULE_H_
+#define MODULES_30_RULE_INCLUDE_30_RULE_H_
+
+#include <vector>
+
+enum class CellState {
+	DEAD,
+	ALIVE
+};
+
+class CellularAutomaton {
+public:
+	CellularAutomaton(const unsigned int rows, const unsigned int cols);
+	CellularAutomaton(const unsigned int rows, const unsigned int cols,
+		const std::vector<CellState>& states);
+	~CellularAutomaton() = default;
+
+	const std::vector <std::vector<CellState>>& get_state() const;
+
+	void iterate(const unsigned int iterations);
+
+private:
+	std::vector<std::vector<CellState>> state;
+	unsigned int cols = 0;
+	unsigned int rows = 0;
+
+	CellState rules(const int row, const int col) const;
+};
+
+#endif // MODULES_30_RULE_INCLUDE_30_RULE_H_
