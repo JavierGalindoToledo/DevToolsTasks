8a0e1c267c63466602c244b1d5f3f8e1840807ef
Javier Galindo <javier.galindot@gmail.com>
2022-05-03 19:55:48 +0300

    version 6 lab 2 (cpplint fix version 4)

--- modules/galindo_30_rule/test/test_galindo_30_rule.cpp
+++ modules/galindo_30_rule/test/test_galindo_30_rule.cpp
@@ -20,26 +20,33 @@ TEST(Javier_Galindo_Cell_Automaton, can_create_cell_class) {
 }
 
 TEST(Javier_Galindo_Cell_Automaton, can_create_cell_class_from_vector) {
-    std::vector<CellState> state{ CellState::DEAD, CellState::ALIVE, CellState::DEAD };
+    std::vector<CellState> state{ CellState::DEAD,
+       CellState::ALIVE, CellState::DEAD };
     ASSERT_NO_THROW(CellularAutomaton(1, 3, state));
 }
 
-TEST(Javier_Galindo_Cell_Automaton, can_iterate_5_times) {
-    std::vector<CellState> state{ CellState::DEAD, CellState::ALIVE, CellState::DEAD };
+TEST(Javier_Galindo_Cell_Automaton,
+    can_iterate_5_times) {
+    std::vector<CellState> state{ CellState::DEAD,
+        CellState::ALIVE, CellState::DEAD };
     CellularAutomaton automat(1, 3, state);
     ASSERT_NO_THROW(automat.iterate(5));
 }
 
-TEST(Javier_Galindo_Cell_Automaton, can_create_2_equal_auto) {
-    std::vector<CellState> state{ CellState::DEAD, CellState::DEAD,  CellState::DEAD };
+TEST(Javier_Galindo_Cell_Automaton,
+    can_create_2_equal_auto) {
+    std::vector<CellState> state{ CellState::DEAD,
+         CellState::DEAD,  CellState::DEAD };
     CellularAutomaton automat(1, 3, state);
     CellularAutomaton automat2(1, 3, state);
 
     ASSERT_EQ(automat.get_state(), automat2.get_state());
 }
 
-TEST(Javier_Galindo_Cell_Automaton, can_use_001_rule) {
-    std::vector<CellState> state{ CellState::DEAD, CellState::DEAD, CellState::ALIVE };
+TEST(Javier_Galindo_Cell_Automaton,
+    can_use_001_rule) {
+    std::vector<CellState> state{ CellState::DEAD,
+        CellState::DEAD, CellState::ALIVE };
     CellularAutomaton automat(2, 3, state);
     automat.iterate(1);
     state = { CellState::DEAD, CellState::ALIVE, CellState::DEAD };
@@ -47,8 +54,10 @@ TEST(Javier_Galindo_Cell_Automaton, can_use_001_rule) {
     ASSERT_EQ(state, automat.get_state()[1]);
 }
 
-TEST(Javier_Galindo_Cell_Automaton, can_use_010_rule) {
-    std::vector<CellState> state{ CellState::DEAD, CellState::ALIVE, CellState::DEAD };
+TEST(Javier_Galindo_Cell_Automaton,
+    can_use_010_rule) {
+    std::vector<CellState> state{ CellState::DEAD,
+       CellState::ALIVE, CellState::DEAD };
     CellularAutomaton automat(2, 3, state);
     automat.iterate(1);
     state = { CellState::DEAD, CellState::ALIVE, CellState::DEAD };
